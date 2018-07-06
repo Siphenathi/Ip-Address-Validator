c646ad8f2fc641f59719d2e5090f5cd6c515b2ad
Siphenathi Pantshwa <SiphenathiP@SAHOMELOANS.COM>
2018-07-06 12:02:17 +0200

    Done

---
+++ IpAddressValidation.Logic/IpAddressValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IpAddressValidation.Logic
+{
+    public class IpAddressValidator
+    {
+        public IpAddressValidator()
+        {
+        }
+
+        public bool ValidateIpv4Address(string ipAddress)
+        {
+            if (string.IsNullOrWhiteSpace(ipAddress) || ipAddress.EndsWith("."))
+            {
+                return false;
+            }
+
+            int startPosition = 0;
+            int numberOfDots = 0;
+
+            for (var x = 0; x < ipAddress.Length; x++)
+            {
+                if (ipAddress[x].ToString() == ".")
+                {
+                    startPosition = x;
+                    numberOfDots++;
+                }
+            }
+
+            var lastDigit = Convert.ToInt32(ipAddress.Substring(startPosition + 1));
+
+            if (lastDigit == 0 || lastDigit == 255 || numberOfDots != 3)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ IpAddressValidation.Test/IpAddressValidatorTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using IpAddressValidation.Logic;
+
+namespace IpAddressValidation.Test
+{
+    [TestFixture]
+    public class IpAddressValidatorTests
+    {
+
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("1.1.1.")]
+        public void ValidateIpv4Address_GivenInvalidIpAddress_ShouldReturnFalse(string ipAddress)
+        {
+            //Arrange
+            var expected = false;
+
+            var sut = new IpAddressValidator();
+
+            //Act
+            var actual = sut.ValidateIpv4Address(ipAddress);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase("0.0.0.0")]
+        [TestCase("255.255.255.255")]
+        [TestCase("10.0.1")]
+        [TestCase("1.1.1.0")]
+        public void ValidateIpv4Address_GivenValidIpAddressThatCannotBeUsedAsHostAddress_ShouldReturnFalse(string ipAddress)
+        {
+            //Arrange
+            var expected = false;
+
+            var sut = new IpAddressValidator();
+
+            //Act
+            var actual = sut.ValidateIpv4Address(ipAddress);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1.1.1.1")]
+        [TestCase("192.168.1.1")]
+        [TestCase("10.0.0.1")]
+        [TestCase("127.0.0.1")]
+        public void ValidateIpv4Address_GivenValidIpAddress_ShouldReturnTrue(string ipAddress)
+        {
+            //Arrange
+            var expected = true;
+
+            var sut = new IpAddressValidator();
+
+            //Act
+            var actual = sut.ValidateIpv4Address(ipAddress);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+    }
+}
